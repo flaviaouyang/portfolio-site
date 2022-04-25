10db508e378922427a54bbdfaa3c3aa11076dc2c
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-25 07:52:57 -0400

    update hobby

--- src/components/about-me/hobby.js
+++ src/components/about-me/hobby.js
@@ -6,10 +6,10 @@ const Hobby = () => {
       <br />
       &emsp;Here are some movies and books that I recommend:
       <ul>
-          <li><a href="https://mubi.com/films/milou-en-mai">Milou en mai</a>, by Louis Malle</li>
-          <li><a href="https://mubi.com/films/zero-fucks-given">RIEN À FOUTRE</a>, by Julie Lecoustre</li>
-          <li><a href="https://www.newyorker.com/magazine/1992/07/20/the-hammer-and-the-nail">Native son</a>, by Richard Wright</li>
-          <li><a href="https://www.newyorker.com/books/page-turner/the-greatest-american-novel-youve-never-heard-of">Stoner</a>, by John Edward Williams</li>
+          <li><a href="https://mubi.com/films/milou-en-mai">Milou en mai&#8599;</a>, by Louis Malle</li>
+          <li><a href="https://mubi.com/films/zero-fucks-given">RIEN À FOUTRE&#8599;</a>, by Julie Lecoustre</li>
+          <li><a href="https://www.newyorker.com/magazine/1992/07/20/the-hammer-and-the-nail">Native son&#8599;</a>, by Richard Wright</li>
+          <li><a href="https://www.newyorker.com/books/page-turner/the-greatest-american-novel-youve-never-heard-of">Stoner&#8599;</a>, by John Edward Williams</li>
           <li>The Rabbit Series, by John Updike</li>
           <li>Almost all poems by Charles Bukowski (I know, i know...)</li>
 
