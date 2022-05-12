7648dea0070c14763d91cd6365a4df33efdb8bce
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-05-12 18:26:30 -0400

    delete a bad movie

--- src/components/about-me/hobby.js
+++ src/components/about-me/hobby.js
@@ -7,7 +7,6 @@ const Hobby = () => {
       &emsp;Here are some movies and books that I recommend:
       <ul>
           <li><a href="https://mubi.com/films/milou-en-mai">Milou en mai&#8599;</a>, by Louis Malle</li>
-          <li><a href="https://mubi.com/films/zero-fucks-given">RIEN À FOUTRE&#8599;</a>, by Julie Lecoustre</li>
           <li><a href="https://www.newyorker.com/magazine/1992/07/20/the-hammer-and-the-nail">Native son&#8599;</a>, by Richard Wright</li>
           <li><a href="https://www.newyorker.com/books/page-turner/the-greatest-american-novel-youve-never-heard-of">Stoner&#8599;</a>, by John Edward Williams</li>
           <li>The Rabbit Series, by John Updike</li>
